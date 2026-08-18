2a522851b2347cd6cdf399180719ce606d09cad7
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-08-18 22:58:49 +0800

    feat(env-validation): register env validation deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { envValidationSchema } from './config/env.validation';
 import { LoggerModule } from './infra/logger/logger.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), LoggerModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+      validationOptions: { abortEarly: false },
+    }),
+    LoggerModule,
+    AuthModule,
+  ],
   providers: [],
 })
 export class AppModule {}
