4d545f880c754d327d75f45c4b4dc8ef1a40873b
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-08-19 03:03:52 +0800

    infra(exception-filter): add HttpExceptionFilter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './modules/auth/auth.module';
 import { envValidationSchema } from './config/env.validation';
 import { LoggerModule } from './infra/logger/logger.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 @Module({
   imports: [
@@ -16,6 +17,9 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
     LoggerModule,
     AuthModule,
   ],
-  providers: [{ provide: APP_INTERCEPTOR, useClass: ResponseInterceptor }],
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class AppModule {}
