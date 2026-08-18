af7b031ab724f5efc0432656d9470e3a630746cf
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-08-19 03:26:04 +0800

    chore(public-decorator): move to common

--- src/modules/auth/guards/firebase-auth.guard.ts
+++ src/modules/auth/guards/firebase-auth.guard.ts
@@ -1,8 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '@/common/decorators/public.decorator';
 import { AuthService } from '../auth.service';
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class FirebaseAuthGuard implements CanActivate {
