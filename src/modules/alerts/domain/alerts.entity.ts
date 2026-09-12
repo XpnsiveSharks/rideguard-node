eaaa88209af1619000fec7027671c7aea68bee0e
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-12 23:09:45 +0800

    feat: add Alert entity with validation and utility methods

---
+++ src/modules/alerts/domain/alerts.entity.ts
@@ -0,0 +1,51 @@
+import { trimmed } from '@/common/helpers/trimmed';
+import { BadRequestException } from '@nestjs/common';
+
+export class Alert {
+  constructor(public readonly alertFields: AlertFields) {}
+
+  public static create(alertFields: AlertFields): Alert {
+    const { deviceId, message, imageUrl, timeStamp, isFalseAlarm, isSeen } = alertFields;
+    const trimmedMessage = trimmed(message, 'message');
+
+    return new Alert({
+      deviceId: deviceId,
+      message: trimmedMessage,
+      imageUrl: imageUrl,
+      timeStamp,
+      isFalseAlarm,
+      isSeen,
+    });
+  }
+
+  getMessage(): string {
+    if (!this.alertFields.message) throw new BadRequestException('Message field is required');
+    return this.alertFields.message;
+  }
+
+  getImageUrl(): string | null | undefined {
+    return this.alertFields.imageUrl;
+  }
+
+  getTimeStamp(): Date {
+    if (!this.alertFields.timeStamp) throw new BadRequestException('Timestamp field is required');
+    return this.alertFields.timeStamp;
+  }
+
+  getIsFalseAlarm(): boolean {
+    return this.alertFields.isFalseAlarm;
+  }
+
+  getIsSeen(): boolean {
+    return this.alertFields.isSeen;
+  }
+}
+
+export type AlertFields = {
+  deviceId: string;
+  message: string;
+  imageUrl?: string | null | undefined;
+  timeStamp: Date;
+  isFalseAlarm: boolean;
+  isSeen: boolean;
+};
