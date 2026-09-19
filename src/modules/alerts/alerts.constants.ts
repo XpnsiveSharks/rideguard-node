f8081e3501340bbd16d67106004f00c95bd5508c
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-20 01:44:03 +0800

    feat: add constants for alert and device events, and realtime channels

---
+++ src/modules/alerts/alerts.constants.ts
@@ -0,0 +1,4 @@
+export const ALERT_EVENTS = {
+  ALERT_CREATED: 'alert.created',
+  ALERT_FAILED: 'alert.failed',
+} as const;
---
+++ src/modules/devices/device.constants.ts
@@ -0,0 +1,5 @@
+export const DEVICE_EVENTS = {
+  DEVICE_CREATED: 'device.created',
+  DEVICE_ASSIGNED: 'device.assigned',
+  DEVICE_ACTIVATED: 'device.activated',
+} as const;
