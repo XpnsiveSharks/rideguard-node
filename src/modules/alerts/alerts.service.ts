17fd2f60938b0ccae4e46ebb79ef02f0cbf42679
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-20 01:45:02 +0800

    feat: refactor createAlert method to include logging and return created alert

--- src/modules/alerts/alerts.service.ts
+++ src/modules/alerts/alerts.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { AlertsRepository } from './infrastructure/alerts.repository';
-import { Alert } from './domain/alerts.entity';
-
+import { Alert, AlertFields } from './domain/alerts.entity';
+import { ALERT_EVENTS } from './alerts.constants';
+import { PinoLogger } from 'nestjs-pino';
 @Injectable()
 export class AlertsService {
-  constructor(private readonly alertsRepository: AlertsRepository) {}
+  constructor(
+    private readonly alertsRepository: AlertsRepository,
+    private readonly logger: PinoLogger,
+  ) {}
 
-  async createAlert(alert: Alert): Promise<void> {
-    await this.alertsRepository.save(alert);
+  async createAlert(alert: AlertFields): Promise<Alert> {
+    const newAlert = Alert.create(alert);
+    await this.alertsRepository.save(newAlert);
+
+    this.logger.info(
+      { event: ALERT_EVENTS.ALERT_CREATED },
+      `Alert created: ${JSON.stringify(newAlert)}`,
+    );
+
+    return newAlert;
   }
 }
