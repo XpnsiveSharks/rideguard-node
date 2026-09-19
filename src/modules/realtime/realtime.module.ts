3e44479c9e970039e117667c765ecf4a8d495788
SharksFinn <aban.marynelle.tesoro@gmail.com>
2026-09-20 01:46:12 +0800

    feat: make DevicesModule global and update RealtimeModule providers

--- src/modules/realtime/realtime.module.ts
+++ src/modules/realtime/realtime.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RealtimeController } from './realtime.controller';
-import { RealtimeService } from './realtime.service';
 import { InferenceSubscriberService } from './service/inference-subscriber.service';
 import { InferenceHandlingService } from './service/inference-handling.service';
 import { AlertsModule } from '../alerts/alerts.module';
+import { RealtimePublisherService } from './service/realtime-publishing.service';
 
 @Module({
   imports: [AlertsModule],
   controllers: [RealtimeController],
-  providers: [RealtimeService, InferenceSubscriberService, InferenceHandlingService],
+  providers: [RealtimePublisherService, InferenceSubscriberService, InferenceHandlingService],
 })
 export class RealtimeModule {}
